9e04be523d90119814885d9cce02439defa1adac
sarthakturkar <sarthak@sakarrobotics.com>
2024-11-29 14:57:37 +0530

    deploy hadapsar

--- test/lisa.sh
+++ test/lisa.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define directories
-BACKEND_DIR=~/lisa/test/demo_baumer/dashboard/backend
-DASHBOARD_DIR=~/lisa/test/demo_baumer/dashboard
+BACKEND_DIR=~/home/sr10/Documents/lisa/test/hadapsar/backend/double
+DASHBOARD_DIR=~/home/sr10/Documents/lisa/test/hadapsar
 
 # Function to open a new terminal and execute a command
 open_terminal() {
@@ -11,15 +11,15 @@ open_terminal() {
 
 
 # Navigate to the deploy folder and run the Python file for connecting to PLC in a new terminal
-open_terminal "cd \"$BACKEND_DIR\" && python3 plc.py"
+open_terminal "cd \"$BACKEND_DIR\" && python3 5secon_Off.py"
 
 # Wait for a few seconds to ensure the PLC connection script starts properly
 sleep 5
 
 # Start the backend server in a new terminal
-open_terminal "cd \"$BACKEND_DIR\" && python3 rejection_database.py"
+open_terminal "cd \"$BACKEND_DIR\" && python3 main.py"
 
-sleep 15
+sleep 10
 
 # Start the dashboard server in a new terminal
 open_terminal "cd \"$DASHBOARD_DIR\" && npm run electron"
